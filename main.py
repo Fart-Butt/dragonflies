2ea25c0bd26c20fa6f07da3e87e51c46f91fc390
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2025-11-08 18:45:39 -0500

    first post

---
+++ main.py
@@ -0,0 +1,23 @@
+from mcrcon import MCRcon
+import os
+import MySQLdb
+import datetime
+
+killed=0
+with MCRcon(os.environ['RCON_HOST'], os.environ['RCON_PASSWORD']) as mcr:
+    resp=mcr.command("/kill @e[name='Dragonfly']")
+    if resp.rstrip() == "No entity was found":
+        killed=0
+    elif resp.rstrip().split(" ")[0] == "Killed" and resp.rstrip().split(" ")[1] != "Dragonfly":
+        killed=int(resp.rstrip().split(" ")[1])
+    else:
+        killed=1
+
+dbcon = MySQLdb.connect(host=os.environ['db_host'],
+                        user=os.environ['db_username'],
+                        passwd=os.environ['db_password'],
+                        db=os.environ['db_name']
+                        )
+with dbcon.cursor() as cursor:
+    cursor.execute("insert into `dragonflies` (`dt`, `killed`) VALUES (%s, %s)", (datetime.datetime.now(), killed))
+dbcon.commit()
