2d2b09aba6e6b637873fb460d9678ba7fbaf954c
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2025-11-26 15:01:24 -0500

    added magic shells

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ import os
 import MySQLdb
 import datetime
 
-killed=0
+df_killed=0
 with MCRcon(os.environ['RCON_HOST'], os.environ['RCON_PASSWORD']) as mcr:
     resp=mcr.command("/kill @e[name='Dragonfly']")
     if resp.rstrip() == "No entity was found":
-        killed=0
+        df_killed=0
     elif resp.rstrip().split(" ")[0] == "Killed" and resp.rstrip().split(" ")[1] != "Dragonfly":
-        killed=int(resp.rstrip().split(" ")[1])
+        df_killed=int(resp.rstrip().split(" ")[1])
     else:
-        killed=1
+        df_killed=1
 
-if killed > 0:
+ms_killed=0
+with MCRcon(os.environ['RCON_HOST'], os.environ['RCON_PASSWORD']) as mcr:
+    resp=mcr.command("/kill @e[name='entity.reliquified_ars_nouveau.magic_shell']")
+    if resp.rstrip() == "No entity was found":
+        ms_killed=0
+    elif resp.rstrip().split(" ")[0] == "Killed" and resp.rstrip().split(" ")[1] != "entities":
+        ms_killed=int(resp.rstrip().split(" ")[1])
+    else:
+        ms_killed=1
+
+
+if df_killed > 0 or ms_killed > 0:
     with MCRcon(os.environ['RCON_HOST'], os.environ['RCON_PASSWORD']) as mcr:
-        resp=mcr.command("/tell @a ZAP! buttbot zapped {} dragonflies".format(killed))
+        resp=mcr.command("/tell @a ZAP! buttbot zapped {} dragonflies and {} magic shells".format(df_killed, ms_killed))
+
+
+
+
 
 dbcon = MySQLdb.connect(host=os.environ['db_host'],
                         user=os.environ['db_username'],
@@ -23,5 +38,8 @@ dbcon = MySQLdb.connect(host=os.environ['db_host'],
                         db=os.environ['db_name']
                         )
 with dbcon.cursor() as cursor:
-    cursor.execute("insert into `dragonflies` (`dt`, `killed`) VALUES (%s, %s)", (datetime.datetime.now(), killed))
+    cursor.execute("insert into `dragonflies` (`dt`, `killed`) VALUES (%s, %s)", (datetime.datetime.now(), df_killed))
+    cursor.execute("insert into `magic_shells` (`dt`, `killed`) VALUES (%s, %s)", (datetime.datetime.now(), ms_killed))
 dbcon.commit()
+
+
